0dd1b262443f899b38318d3fdb4858738c662a83
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2021-03-28 09:57:06 -0300

    test(pages) Add SignIn simple page test
    Write test using RNTL
    Reinstall correct AsyncStorage lib
    Fix minor issues for test

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -108,7 +108,7 @@ const SignIn: React.FC = () => {
                 icon="mail"
                 placeholder="E-mail"
                 returnKeyType="next"
-                onSubmitEditing={}
+                onSubmitEditing={() => passwordInputRef.current?.focus()}
               />
               <Input
                 ref={passwordInputRef}
